4613d65d5ec2431e4e10fd4744c06cc277102ea6
mchedlish <mchedlish@yahoo.com>
2018-01-02 19:46:52 +0400

    mitsake corrected

--- colorGame.js
+++ colorGame.js
@@ -16,14 +16,16 @@ var squares = document.querySelectorAll(".square");
 colorDisplay.textContent=pickedColor;
 
 
-for (var i=0; i<squares.length;i++){
+for (var i=0; i<squares.length; i++){
 squares[i].style.backgroundColor=colors[i];
 squares[i].addEventListener("click", function(){
 var clickedColor = this.style.backgroundColor;
 if (clickedColor===pickedColor){
 alert("Correct");
 }
-    else {alert("Wrong");}
+    else {alert("Wrong");
+
+         }
 
 });
 }
