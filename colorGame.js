4d1b49b7ec225221ad4cb1e5c88a2067db875123
mchedlish <mchedlish@yahoo.com>
2018-01-02 19:27:20 +0400

    ok

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,21 @@
 var colors = ["rgb(255, 0, 0)",
              "rgb(255, 255, 0)",
               "rgb(0, 255, 0)",
-             "rgb(0, 255, 255)",
+              "rgb(0, 255, 255)",
               "rgb(0, 0, 255)",
               "rgb(255, 0, 255)"
 
              ];
+
 var pickedColor = colors[3];
+
 var colorDisplay = document.getElementById("colorDisplay");
+
+
 var squares = document.querySelectorAll(".square");
 colorDisplay.textContent=pickedColor;
+
+
 for (var i=0; i<squares.length;i++){
 squares[i].style.backgroundColor=colors[i];
 squares[i].addEventListener("click", function(){
@@ -19,5 +25,6 @@ alert("Correct");
 }
     else {alert("Wrong");}
 
-})
+});
 }
+
