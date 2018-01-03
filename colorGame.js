1d33802869af3c983367d3bd968089a713e0cd7b
mchedlish <mchedlish@yahoo.com>
2018-01-03 20:58:55 +0400

    reset button added

--- colorGame.js
+++ colorGame.js
@@ -1,11 +1,21 @@
 var colors = generateRandomColors(6);
 
-var pickedColor = pickColor();
+var pickedColor = pickColor ();
 var colorDisplay = document.getElementById("colorDisplay");
 var squares = document.querySelectorAll(".square");
 colorDisplay.textContent=pickedColor;
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", function(){
+colors = generateRandomColors(6);
+pickedColor = pickColor();
+colorDisplay.textContent = pickedColor;
+for (var i=0; i<squares.length; i++){
+squares[i].style.backgroundColor=colors[i];}
+h1.style.backgroundColor="#232323";
+
+});
 
 for (var i=0; i<squares.length; i++){
 squares[i].style.backgroundColor=colors[i];
@@ -13,6 +23,7 @@ squares[i].addEventListener("click", function(){
 var clickedColor = this.style.backgroundColor;
 if (clickedColor===pickedColor){
 messageDisplay.textContent = "Correct";
+resetButton.textContent="Play Again?";
 changeColors(clickedColor);
 h1.style.backgroundColor=clickedColor;
 }
@@ -30,7 +41,7 @@ squares[i].style.backgroundColor=color;
 }
 
 }
-function pickColor(){
+function pickColor (){
 var random = Math.floor(Math.random()*colors.length);
     return colors[random];
 }
